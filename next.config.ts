f5139d194ffdbaa13e42cfac1746ce24be21719e
tadat1405 <tadat1405@gmail.com>
2026-09-13 23:47:12 +0700

    feat: complete setup and deploy to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let basePath = "";
+let assetPrefix = "";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "") || "KeTi";
+  basePath = `/${repo}`;
+  assetPrefix = `/${repo}/`;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: basePath || undefined,
+  assetPrefix: assetPrefix || undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
